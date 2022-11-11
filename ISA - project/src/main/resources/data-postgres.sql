5e49df0563b33e47eef86685c47267d8b66a0353
cikii555 <cvetanaciki555@gmail.com>
2022-11-11 10:00:09 +0100

    Add data for some tables for sql script

--- ISA - project/src/main/resources/data-postgres.sql
+++ ISA - project/src/main/resources/data-postgres.sql
@@ -1,3 +1,15 @@
 insert  into  address (country,city,street,street_number,lon,lat) values ('Srbija','Novi Sad','Balzakova','58','43.27','44.23');
 insert  into  address (country,city,street,street_number,lon,lat) values ('Srbija','Novi Sad','Sekspirova','23','43.27','44.23');
 insert  into  address (country,city,street,street_number,lon,lat) values ('Srbija','Novi Sad','Alekse Santica','12','43.27,','44.23');
+
+
+
+
+insert into blood_bank (blood_type,quantity,blood_transfusion_center_id) values ('A+',20,1)
+insert into blood_bank (blood_type,quantity,blood_transfusion_center_id) values ('B+',20,1)
+insert into blood_bank (blood_type,quantity,blood_transfusion_center_id) values ('0-',20,1)
+insert into blood_bank (blood_type,quantity,blood_transfusion_center_id) values ('AB-',20,1)
+
+
+insert into work_hours (start_time,end_time) values ('8:00','16:00')
+insert into work_hours (start_time,end_time) values ('9:00','16:00')
