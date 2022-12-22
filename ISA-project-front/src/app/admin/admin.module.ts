f583330a091f88c729f4ff9062cd37da137b586c
Dusan-Stanimirovic <dussanstanimirovic@gmail.com>
2022-12-22 17:43:59 +0100

    fixed date Picker

--- ISA-project-front/src/app/admin/admin.module.ts
+++ ISA-project-front/src/app/admin/admin.module.ts
@@ -69,7 +69,8 @@ const routes: Routes = [
     EditProfileComponent,
     TransfusionCenterPanelComponent,
     BloodBanksComponent,
-    BloodDonationAppointmentComponent
+    BloodDonationAppointmentComponent,
+    CreateNewAppointmentComponent
   ],
   imports: [
     CommonModule,
@@ -81,6 +82,8 @@ const routes: Routes = [
     HttpClientModule,
     MatCardModule,
     MatSidenavModule,
+    MaterialModule,            // <----- this module will be deprecated in the future version.
+    MatDatepickerModule,
     MatDividerModule,
     RouterModule.forChild(routes),
     MatDatepickerModule,
