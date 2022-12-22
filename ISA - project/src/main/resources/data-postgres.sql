c2d950e8f022aed86870911c375111aee1c87669
Dusan-Stanimirovic <dussanstanimirovic@gmail.com>
2022-12-22 13:00:48 +0100

    changed sender mail to isa.hospitall

--- ISA - project/src/main/resources/data-postgres.sql
+++ ISA - project/src/main/resources/data-postgres.sql
@@ -70,7 +70,7 @@ values (nextval('my_seq_gen_user'), 'client@gmail.com', '$2a$10$Se9RxWx8NYpBrCvz
 insert into client (id, email, password, first_name, last_name, address_id, phone_number, jmbg, gender,
 
                     is_active, role_id, penalty, graded_center, occupation, organization_information, filled_out_survey)
-values (nextval('my_seq_gen_user'), 'client1@gmail.com', '$2a$10$Se9RxWx8NYpBrCvzLjxyuul8D6oJhC6.W6srGYniLFqCiP/zOqupq',
+values (nextval('my_seq_gen_user'), 'dussanstanimirovic@gmail.com', '$2a$10$Se9RxWx8NYpBrCvzLjxyuul8D6oJhC6.W6srGYniLFqCiP/zOqupq',
         'Marko', 'Markovic', 1, '0601231231', 439545, 'MALE', true, 2 , 0, false, 'student', 'ftn', false);
 
 INSERT INTO public.appointment(
